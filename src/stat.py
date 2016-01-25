c571e3ddbe529cd71d66cddb366790cdd7ff2157
Xiaotian Wang <wxt1994@gmail.com>
2016-01-24 19:57:24 -0800

    Updated ffshaing metric.

--- src/stat.py
+++ src/stat.py
@@ -450,59 +450,47 @@ def checkFSharMetric():
 	writeCSV('test_fsharing.csv', my_list)
 	print "Data written..."
 
-	# build dictionary for the metric
+	# build dictionary for the metric (corresponding to 32 CPUs)
 	myDict = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], \
 	          8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: [],\
 	          16: [], 17: [], 18: [], 19: [], 20: [], 21: [], 22: [], 23: [], \
 	          24: [], 25: [], 26: [], 27: [], 28: [], 29: [], 30: [], 31: []}
 
-	# gather smallest vlaue for addr of each CPU ID
-
+	# gather value for addr of each CPU ID
 	for i in xrange(235446):
 		if ft2[i] == 1:  # L1 cache
 			myDict[ft4[i]].append(ft1[i])
 
+	# calculate mean address for each CPU
 	means = [0.0]*32
 	for i in xrange(32):
 		addrs = myDict[i]
 		means[i] = sum(addrs) / float(len(addrs))
 		print "mean[", i, "]: ", means[i]
 
-	for i in xrange(int(len(myDict[0]))):
-		if myDict[0][i] < means[0]:
-			print "round: ", i, " addr: ", myDict[0][i]
+	# for test max and min addr in the first CPU
 	print max(myDict[0])
 	print min(myDict[0])
 
-	md_max = max(myDict[0])
-	md_min = min(myDict[0])
-
-	# fuzzy false sharing metric
-	# k is assigned to be 4 in this case
-	# by plotting out addresses, we could
-	# see there are roughly 4 clusters
+	# fuzzy false sharing metric, k is assigned to be 4 in this case by
+	# plotting out addresses, we could see there are roughly 4 clusters
 	k = 4
-	avrg_addr = [0.0]*32*k # mean for each cluster in 32 CPUs
-	std_addr = [0.0]*32 # standard deviation for addresses of each CPU
-	calc_distr = [0.0]*32 # calculated normal distribution probability
+	avrg_addr = [0.0]*32*k # mean for each cluster in 32 CPUs, k copies
+	std_addr = [0.0]*32*k # standard deviation for addresses of each CPU
+	calc_distr = [0.0]*32 # calculated gaussian distribution probability
 
 	for l in xrange(32):
-		CPU_d = [] # CPU data containing memory address
+		CPU_d = [] # copying addr data for each CPU
 		for i in xrange(int(len(myDict[l]))):
 			CPU_d.append([myDict[l][i]])
 
-		# sns.distplot(CPU_d, bins=250)
-		# plt.hist(CPU_d, 20)
-
 		# kmeans clustering for address
-		y = []
+		y = [] # array with cluster assignments
 		y, centers, labels_unique = useKMeans(CPU_d, y, n_clusters=k)
-		# print labels_unique
-		# print centers
 
 		# calculate mean addresses for each cluster
-		sample_d = [0.0]*k # 2
-		sig_d = [0.0]*k
+		sample_d = [0.0]*k # mean addr for the cluster
+		sig_d = [0.0]*k # significance metric for each cluster
 		for i in xrange(k):
 			sum_k = 0.0
 			count_k = 0
@@ -513,97 +501,96 @@ def checkFSharMetric():
 			mean_k = sum_k / count_k
 			sample_d[i] = mean_k
 
-		# if sample_d[0] > sample_d[1]:
-		# 	avrg_addr[l*2] = sample_d[0]
-		# 	avrg_addr[l*2+1] = sample_d[1]
-		# else:
-		# 	avrg_addr[l*2] = sample_d[1]
-		# 	avrg_addr[l*2+1] = sample_d[0]
-
-		# assign mean address to the variable
-		# k consecutive same mean addresses for each CPU
+		# assign mean address of each cluster to the variable
 		for i in xrange(k):
 			avrg_addr[l*k+i] = sample_d[i]
 
+		# assign significance metric each cluster to the variable
 		for i in xrange(k):
 			sig_d[i] = (sample_d[0]-sample_d[i]) / sample_d[i] * 100.0
 
-		# calculate standard deviation
-		stddev = 0
-		for j in xrange(int(len(myDict[l]))):
-			stddev += (myDict[l][j] - sample_d[i])**2
-		stddev = stddev*1.0 / len(myDict[l])
-
-		std_addr[l] = (stddev)**0.5
+		# calculate standard deviation for each cluster of a CPU
+		for i in xrange(k):
+			dev = 0.0
+			stddev = 0.0
+			for j in xrange(int(len(myDict[l]))):
+				if y[j] == i:
+					dev += (myDict[l][j] - avrg_addr[l*k+i])**2
+			stddev = dev**0.5
+			std_addr[l*k+i] = stddev
 
 		print "cpu: ", l
-
 		print sample_d
-		print sig_d
 
+	print " "
 	print avrg_addr
 	print " "
 	print "standard deviation: "
 	print std_addr
 
-	for x in xrange(1,10):
-		pass
-
-	# calculate # of elements stored in the dic
+	# calculate number of elements stored in the dic
 	count_d = 0
 	for i in xrange(32):
 		count_d += int(len(myDict[i]))
 	print count_d
 
 	# create list for the metric
-	ffsharing = [3.0]*235446
+	ffsharing = [0.0]*235446
 	counter  = 0.0
+
 	for i in xrange(235446):
 		if ft2[i] == 1:  # L1 cache
-		    # calculate raw CPU index from the memory address
+		    # calculate raw CPU index by smallest distance
 			region = closest_addr_region(avrg_addr, ft1[i])
-			# convert raw CPU index to CPU index
-			mregion = int(region)/k
-			start_p = 0
-			theMean = avrg_addr[4*ft4[i]+1]
-			theSTDDEV = std_addr[ft4[i]]
-			theMean_twin = avrg_addr[4*twin_proc(ft4[i])+1]
-			theSTDDEV_twin = std_addr[twin_proc(ft4[i])]
 
-			# calculate raw addr distribution for corresponding CPU
-			ffaddr = abs(ft1[i] - theMean)*1.0 / theSTDDEV
-			ffaddr_twin = abs(ft1[i] - theMean_twin)*1.0 / theSTDDEV_twin
-			ffsharing[i] = ffaddr+ffaddr_twin
+			# convert raw CPU index to CPU cluster index
+			mregion = int(region)/k # calculated cluster index
+			# start_p = 0
+			# theMean = avrg_addr[4*ft4[i]+l] # actual mean value
+			# theSTDDEV = std_addr[4*ft4[i]+l] # actual atandard deviation
+			# theMean_twin = avrg_addr[4*twin_proc(ft4[i])+1]
+			# theSTDDEV_twin = std_addr[twin_proc(ft4[i])]
 
-			counter = ffsharing[i] if ffsharing[i] >= counter else counter
+			# # calculate raw addr distribution for corresponding CPU
+			# ffaddr = abs(ft1[i] - theMean)*1.0 / theSTDDEV
+			# ffaddr_twin = abs(ft1[i] - theMean_twin)*1.0 / theSTDDEV_twin
+			# ffsharing[i] = ffaddr+ffaddr_twin
+
+			# counter = ffsharing[i] if ffsharing[i] >= counter else counter
 
 			# calculate raw time distribution
 			# two acesses are considered to be close in range of 10
-			time_count = 0
-			time_denom = 0
-			if i < 5:
-				time_denom = (i+5)*2
-				for x in xrange(i+5):
-					if ft4[i] == ft4[x]:
-						time_count += 2
-					elif ft4[i] == twin_proc(ft4[x]):
-						time_count += 1
-			elif i >= 5 and i < 235441:
-				time_denom = 20
-				for x in xrange(i-5, i+5):
-					if ft4[i] == ft4[x]:
-						time_count += 2
-					elif ft4[i] == twin_proc(ft4[x]):
-						time_count += 1
-			else:
-				time_denom = (235446-i+5)*2
-				for x in xrange(i-5, 235446):
-					if ft4[i] == ft4[x]:
-						time_count += 2
-					elif ft4[i] == twin_proc(ft4[x]):
-						time_count += 1
+			ffs_metric = 0.0
 
-			time_count = (time_count*3.0 / time_denom)
+			if i < 5:
+				for x in xrange(i+5):
+					temp_region = closest_addr_region(avrg_addr, ft1[x])
+					temp_mregion = int(temp_region)/k
+					if temp_mregion == mregion:
+						if ft4[x] != ft4[i]:
+							ffs_metric += 3
+						elif ft4[x] == twin_proc(ft4[i]):
+							ffs_metric += 1
+			elif i >= 5 and i < 235441:
+				for x in xrange(i-5, i+5):
+					temp_region = closest_addr_region(avrg_addr, ft1[x])
+					temp_mregion = int(temp_region)/k
+					if temp_mregion == mregion:
+						if ft4[x] != ft4[i]:
+							ffs_metric += 3
+						elif ft4[x] == twin_proc(ft4[i]):
+							ffs_metric += 1
+			else:
+				for x in xrange(i-5, 235446):
+					temp_region = closest_addr_region(avrg_addr, ft1[x])
+					temp_mregion = int(temp_region)/k
+					if temp_mregion == mregion:
+						if ft4[x] != ft4[i]:
+							ffs_metric += 3
+						elif ft4[x] == twin_proc(ft4[i]):
+							ffs_metric += 1
+
+			ffsharing[i] = ffs_metric
 
 			# ffsharing[i] += time_count
 
@@ -631,7 +618,7 @@ def checkFSharMetric():
 	# g_1.set_xlabel('sharing catagories (refer to code)')
 	# g_1.set_ylabel('frequencies')
 	# sns.plt.show()
-	print "counter: "+str(counter)
+	# print "counter: "+str(counter)
 
 	for x in xrange(1,100):
 		print ffsharing[x]
