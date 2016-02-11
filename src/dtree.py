8075dbb9d733f1b57b474a0458399764bf3ad844
王小天 <wxt1994@gmail.com>
2016-02-10 17:44:42 -0800

    Updated decision tree method.

--- src/dtree.py
+++ src/dtree.py
@@ -29,39 +29,46 @@ data_lat = Data()
 data_src = Data()
 data_shar = Data()
 data_cputid = Data()
-cpu = Data()
+data_ffs = Data()
 
 data_lat.load('test_latency.csv',ify=False)
 data_src.load('test_data_src.csv',ify=False)
 data_shar.load('test_sharmetric.csv',ify=False)
 data_cputid.load('test_tidupumetric.csv',ify=False)
-cpu.load('test_cpu.csv',ify=False)
+data_ffs.load('test_ffsharing.csv',ify=False)
+
+preview('test_latency.csv')
 
 X_1,y_1 = data_lat.getXy()
 X_2,y_2 = data_src.getXy()
-X_3,y_3 = cpu.getXy()
+X_3,y_3 = data_ffs.getXy()
 X_4,y_4 = data_shar.getXy()
 X_5,y_5 = data_cputid.getXy()
 
 latency = []
 data_src = []
-CPU = []
+ffshar = []
 shar = []
 cputid = []
 
 for i in xrange(235446):
-    if float(X_1[i][0]) < 120:
+    if float(X_1[i][0]) < 500:
         latency.append(float(X_1[i][0]))
         data_src.append(float(X_2[i][0]))
-        CPU.append(float(X_3[i][0]))
+        ffshar.append(float(X_3[i][0]))
         shar.append(float(X_4[i][0]))
         cputid.append(float(X_5[i][0]))
 
-my_list = zip(data_src,CPU,shar,cputid)
+my_list = zip(data_src,ffshar,shar,cputid)
 
 # catagorize latency
+max_lat = 0
 for i in xrange(len(latency)):
 	temp = latency[i]
-	latency[i] = temp//30
+	if latency[i] >= max_lat:
+		max_lat = latency[i]
+	latency[i] = temp//60
+
+print max_lat
 
 useDecisionTree(my_list, latency)
